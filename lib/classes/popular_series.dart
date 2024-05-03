df536e5a77a0ddbf92e911ae0065322325da8fb5
kamilateofilo <123755464+kamilateofilo@users.noreply.github.com>
2024-05-03 19:37:37 -0300

    Feat: ajust

--- lib/classes/popular_series.dart
+++ lib/classes/popular_series.dart
@@ -1,11 +1,30 @@
 class PopularSeries {
-  final String id;
-  final String title;
-  final String image; 
+  final String originalName;
+  final String posterPath;
+  final String firstAirDate;
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final List<String> originCountry;
+  final String originalLanguage;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final double voteAverage;
+  final int voteCount;
 
   PopularSeries({
-    required this.id,
-    required this.title,
-    required this.image,
+    required this.originalName = "",
+    required this.posterPath = "",
+    required this.firstAirDate
+    this.adult = false,
+    this.backdropPath = "",
+    this.genreIds = const [],
+    this.originCountry = const [],
+    this.originalLanguage = "",
+    this.overview = "",
+    this.popularity = 0.0,
+    this.voteAverage = 0.0,
+    this.voteCount = 0,
   });
 }

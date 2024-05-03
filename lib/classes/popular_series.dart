6a26d4876ab0678a0e6cbe8b7e028de867248f15
kamilateofilo <123755464+kamilateofilo@users.noreply.github.com>
2024-05-03 16:15:07 -0300

    Feat: class creation
    Series

---
+++ lib/classes/popular_series.dart
@@ -0,0 +1,11 @@
+class PopularSeries {
+  final String id;
+  final String title;
+  final String image; 
+
+  PopularSeries({
+    required this.id,
+    required this.title,
+    required this.image,
+  });
+}

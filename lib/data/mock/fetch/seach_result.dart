f889504cb4e7202268716e7dca0a710e5ee1df33
jcr04 <joaocarlosreboucas15@gmail.com>
2024-05-03 19:45:34 -0300

    Update seach_result.dart

--- lib/data/mock/fetch/seach_result.dart
+++ lib/data/mock/fetch/seach_result.dart
@@ -5,62 +5,50 @@ void main() {
   {
     "popularMovies": [
       {
-        "popularity": 2157.099,
-        "title": "Macaco vs. Máquina",
-        "overview": "Reconhecendo o poder destrutivo de seu macaco gigante, o exército americano decide criar um macaco robô. Mas o primeiro teste acaba dando terrivelmente errado e obriga o exército a soltar o animal de verdade para enfrentar a máquina.",
-        "poster_path": "/isXDRoaeOm7BJVXVwPzPTnKWKqi.jpg"
-      },
-      // Adicione mais filmes conforme necessário
+        "popularity": string,
+        "title": "string",
+        "overview": "string",
+        "poster_path": "/caminho/do/poster.jpg"
+      }
     ],
     "popularTvSeries": [
       {
-        "popularity": 2157.099,
+        "popularity": String,
         "title": "Nome da Série",
         "overview": "Descrição da série",
         "poster_path": "/caminho/do/poster.jpg"
-      },
-      // Adicione mais séries conforme necessário
+      }
     ],
     "trending": [
       {
-        "popularity": 2157.099,
+        "popularity": String,
         "title": "Título do Item Trending",
         "overview": "Descrição do item trending",
         "poster_path": "/caminho/do/poster.jpg"
-      },
-      // Adicione mais itens trending conforme necessário
+      }
     ]
   }
   ''';
 
   Map<String, dynamic> data = jsonDecode(jsonString);
 
-  // Processar filmes populares
-  List<dynamic> popularMovies = data['popularMovies'];
-  for (var movie in popularMovies) {
-    print('Título do Filme: ${movie['title']}');
-    print('Descrição: ${movie['overview']}');
-    print(
-        'Poster URL: https://image.tmdb.org/t/p/w500/${movie['poster_path']}');
-    print('');
+  // Função para processar metadados
+  void processMetadata(List<dynamic> items) {
+    for (var item in items) {
+      print('Título: ${item['title']}');
+      print('Descrição: ${item['overview']}');
+      print(
+          'Poster URL: https://image.tmdb.org/t/p/w500/${item['poster_path']}');
+      print('');
+    }
   }
+
+  // Processar filmes populares
+  processMetadata(data['popularMovies']);
 
   // Processar séries populares
-  List<dynamic> popularTvSeries = data['popularTvSeries'];
-  for (var series in popularTvSeries) {
-    print('Título da Série: ${series['title']}');
-    print('Descrição: ${series['overview']}');
-    print(
-        'Poster URL: https://image.tmdb.org/t/p/w500/${series['poster_path']}');
-    print('');
-  }
+  processMetadata(data['popularTvSeries']);
 
   // Processar itens trending
-  List<dynamic> trendingItems = data['trending'];
-  for (var item in trendingItems) {
-    print('Título do Item Trending: ${item['title']}');
-    print('Descrição: ${item['overview']}');
-    print('Poster URL: https://image.tmdb.org/t/p/w500/${item['poster_path']}');
-    print('');
-  }
+  processMetadata(data['trending']);
 }

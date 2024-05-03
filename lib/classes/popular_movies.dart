8b49bf27290d5d729200af3416b33ef1a9ab4d1c
kamilateofilo <123755464+kamilateofilo@users.noreply.github.com>
2024-05-03 19:49:11 -0300

    Feat: Ajuste
    Ajuste e adição de novos atributos, de acordo com o .json

--- lib/classes/popular_movies.dart
+++ lib/classes/popular_movies.dart
@@ -1,11 +1,31 @@
 class PopularMovie {
-  final String id;
   final String title;
-  final String image; 
+  final String posterPath;
+  final String releaseDate;
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
 
   PopularMovie({
-    required this.id,
     required this.title,
-    required this.image,
+    required this.posterPath,
+    required this.releaseDate,
+    this.adult = false,
+    this.backdropPath = "",
+    this.genreIds = const [],
+    this.originalLanguage = "",
+    this.originalTitle = "",
+    this.overview = "",
+    this.popularity = 0.0,
+    this.video = false,
+    this.voteAverage = 0.0,
+    this.voteCount = 0,
   });
 }

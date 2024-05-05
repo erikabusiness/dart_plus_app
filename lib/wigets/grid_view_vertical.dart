a4a4ac5e2d9a1764d0f119b01a158e0388bf9bc8
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-05 17:34:43 -0300

    fix: Fix bug in gridview

--- lib/wigets/grid_view_vertical.dart
+++ lib/wigets/grid_view_vertical.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dart_plus_app/classes/popular_movies.dart';
 import 'package:dart_plus_app/classes/popular_series.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WidgetGridViewVertical extends StatelessWidget {
   final List<dynamic> mediaItems;
@@ -12,64 +13,72 @@ class WidgetGridViewVertical extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: const EdgeInsets.all(24),
-      itemCount: mediaItems.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 30,
-        childAspectRatio: 0.55,
-      ),
-      itemBuilder: (context, index) {
-        var media = mediaItems[index];
-        String title = media is PopularMovie
-            ? media.title
-            : (media as PopularSeries).originalName;
-        String posterPath = media.posterPath;
-        String releaseDate = media is PopularMovie
-            ? media.releaseDate
-            : (media as PopularSeries).firstAirDate;
+    return Expanded(
+      child: GridView.builder(
+        padding: const EdgeInsets.all(24),
+        itemCount: mediaItems.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 24,
+          mainAxisSpacing: 16,
+          childAspectRatio: 0.45,
+        ),
+        itemBuilder: (context, index) {
+          var media = mediaItems[index];
+          String title = media is PopularMovie
+              ? media.title
+              : (media as PopularSeries).originalName;
+          String posterPath = media.posterPath;
+          String releaseDate = media is PopularMovie
+              ? media.releaseDate
+              : (media as PopularSeries).firstAirDate;
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://image.tmdb.org/t/p/w500$posterPath'),
-                    fit: BoxFit.cover,
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AspectRatio(
+                aspectRatio: 0.7,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          'https://image.tmdb.org/t/p/w500$posterPath'),
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    releaseDate,
+                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 4),
-              child: Text(
-                releaseDate,
-                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: true,
+                ),
               ),
-            ),
-          ],
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 }

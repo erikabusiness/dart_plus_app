4e713256fee84aeb6a99e9617d5112399d087aa5
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-10 09:27:23 -0300

    feat: Adds horizontal list view for popular TV shows and movies.

---
+++ lib/widgets/list_view_horizontal.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:dart_plus_app/widgets/media_card.dart';
+
+class WidgetListViewHorizontal extends StatelessWidget {
+  final List<dynamic> mediaItems;
+
+  const WidgetListViewHorizontal({
+    super.key,
+    required this.mediaItems,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.2,
+      child: Container(
+        margin: EdgeInsets.zero,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: mediaItems.length,
+          itemBuilder: (BuildContext context, int index) {
+            return AspectRatio(
+              aspectRatio: 0.8,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                ),
+                child: MediaCard(
+                  media: mediaItems[index],
+                  title: null,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      //),
+    );
+  }
+}

cc8655b76d68e95cf3326a361fbfe49cee13e98b
C1337001 Emerson Mendes da Costa <c1337001@interno.bb.com.br>
2024-05-20 22:00:08 -0300

    fix: - display name shadow alignment fix

--- lib/widgets/caroseul.dart
+++ lib/widgets/caroseul.dart
@@ -13,52 +13,50 @@ class WidgetCarousel extends StatelessWidget {
     return CarouselSlider(
       items: mediaItems.map((media) {
         return Container(
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.symmetric(horizontal: 5.0),
-            padding: const EdgeInsets.all(8.0),
-
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Image.network(
-                    'https://image.tmdb.org/t/p/w500${media.backdropPath}',
-                    fit: BoxFit.cover,
-                  ),
+          width: MediaQuery.of(context).size.width,
+          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Image.network(
+                  'https://image.tmdb.org/t/p/w500${media.backdropPath}',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
                 ),
-                Container(
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
                   height: 70,
-                  margin: const EdgeInsets.only(top: 92),
                   decoration: BoxDecoration(
-                    // color: Colors.black.withOpacity(0.5),
                     gradient: LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       colors: [
-                        Colors.black.withOpacity(0), 
-                        Colors.black.withOpacity(0.9)]                    ),
+                        Colors.black.withOpacity(0),
+                        Colors.black.withOpacity(0.9)
+                      ],
+                    ),
                     borderRadius: const BorderRadius.only(
                       bottomLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16)
+                      bottomRight: Radius.circular(16),
                     ),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(left: 5, bottom: 15),
-                      alignment: Alignment.center,
-                      child: WidgetTitleSection(
-                        title: media.title,
-                        textAlign: TextAlign.center,
-                      )
-                    ),
-                  ],
+              ),
+              Positioned(
+                bottom: 15,
+                left: 10,
+                right: 10,
+                child: WidgetTitleSection(
+                  title: media.title,
+                  textAlign: TextAlign.center,
                 ),
-              ],
-            )
+              ),
+            ],
+          ),
         );
       }).toList(),
       options: CarouselOptions(

321bb5c8690ff17d6cc84b3c85d3614a6e23913a
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-24 16:58:06 -0300

    feat: Creates Database for Popular Movies and Popular Series

---
+++ lib/data/database.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+  static const String dbName = 'Media.db';
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+
+    _database = await initDatabase();
+    return _database;
+  }
+
+  Future<Database> initDatabase() async {
+    String path = join(await getDatabasesPath(), dbName);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE PopularMovies (
+            id INTEGER PRIMARY KEY,
+            release_date TEXT, 
+            original_title TEXT,
+            video TEXT,
+            title TEXT,
+            poster_path TEXT,
+            adult TEXT,
+            backdrop_path TEXT,
+            genre_ids TEXT, 
+            original_language TEXT,
+            overview TEXT,
+            vote_average REAL,
+            popularity REAL,
+            vote_count INTEGER
+          )
+        ''');
+        await db.execute('''
+          CREATE TABLE PopularSeries (
+            id INTEGER PRIMARY KEY,
+            release_date TEXT, 
+            original_name TEXT,
+            origin_country TEXT,
+            title TEXT,
+            poster_path TEXT,
+            adult TEXT,
+            backdrop_path TEXT,
+            genre_ids TEXT, 
+            original_language TEXT,
+            overview TEXT,
+            vote_average REAL,
+            popularity REAL,
+            vote_count INTEGER
+          )
+        ''');
+      },
+    );
+  }
+}

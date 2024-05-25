07394ffba457176fb39268f14cf73a00d4413799
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-24 22:52:47 -0300

    feat: condition for favorite

--- lib/models/popular_movies.dart
+++ lib/models/popular_movies.dart
@@ -40,7 +40,7 @@ class PopularMovie extends Media {
       'popularity': popularity,
       'vote_average': voteAverage,
       'vote_count': voteCount,
-      'is_favorite': isFavorite,
+      'is_favorite': isFavorite ? 'true' : 'false',
     };
   }
 

2f07d2b9ab8667939449aa76d4b3208c856cd550
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-06-03 16:39:26 -0300

    feat: verifies the connection and use database

---
+++ lib/data/connection_status.dart
@@ -0,0 +1,11 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+Future<bool> isConnectedToInternet() async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  if (connectivityResult.contains(ConnectivityResult.mobile) ||
+      connectivityResult.contains(ConnectivityResult.wifi)) {
+    return true;
+  } else {
+    return false;
+  }
+}

32667864cefe22d945d7f66cc3cf23e56044e282
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-06-03 13:37:59 -0300

    feat: Ui of genre page
    Co-authored-by: Jaqueline Dreyer <jaquedreyer@users.noreply.github.com>

--- lib/view/catalogo_page.dart
+++ lib/view/catalogo_page.dart
@@ -1,46 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:dart_plus_app/utils/utils_functions.dart';
 import 'package:dart_plus_app/widgets/favorite_icon.dart';
 import 'package:dart_plus_app/widgets/genre_label.dart';
 import 'package:dart_plus_app/widgets/navigation_bar.dart';
 import 'package:dart_plus_app/widgets/title_section.dart';
-import 'package:flutter/material.dart';
 import '../models/media.dart';
 
 class CatalogoPage extends StatefulWidget {
   const CatalogoPage({super.key});
 
   @override
-  State<CatalogoPage> createState() => _CatalagoPageState();
+  State<CatalogoPage> createState() => _CatalogoPageState();
 }
 
-class _CatalagoPageState extends State<CatalogoPage> {
+class _CatalogoPageState extends State<CatalogoPage>
+    with SingleTickerProviderStateMixin {
   int _selectedIndex = 1;
   List<Media> media = [];
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController =
+        TabController(length: UtilsFunctions.genre.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
-        title: const WidgetTitleSection(title: 'Catálogo'),
+        title: const WidgetTitleSection(title: 'Catálogo'),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: UtilsFunctions.genre.values.map((String genre) {
+            return Tab(text: genre);
+          }).toList(),
+        ),
       ),
       body: Column(
         children: [
-          ListView.builder(
-            itemCount: media.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: media[index].posterPath.isNotEmpty
-                    ? Image.network(
-                        'https://image.tmdb.org/t/p/w500${media[index].posterPath}',
-                      )
-                    : const Icon(Icons.movie),
-                title: Text(media[index].title),
-                subtitle: GenreLabelWidget(
-                  media: media[index],
-                  isCompact: true,
-                ),
-              );
-            },
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: UtilsFunctions.genre.keys.map((int genreId) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: media.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        leading: media[index].posterPath.isNotEmpty
+                            ? Image.network(
+                                'https://image.tmdb.org/t/p/w500${media[index].posterPath}',
+                              )
+                            : const Icon(Icons.movie),
+                        title: Text(media[index].title),
+                        subtitle: GenreLabelWidget(
+                          media: media[index],
+                          isCompact: true,
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }).toList(),
+            ),
           ),
         ],
       ),

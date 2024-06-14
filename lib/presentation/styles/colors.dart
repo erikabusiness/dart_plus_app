374899d16a03e3ce523b0a3cf4cda680487262d9
Emerson Mendes <devmendesc@gmail.com>
2024-06-13 21:08:30 -0300

    feat: adicionado a cor de background default

--- lib/presentation/styles/colors.dart
+++ lib/presentation/styles/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomColor {
   static const Color dopaminExtraColor = Color(0xffBEA9EE);
   static const Color defaultTextColor = Color(0XffE3E1EC);
+  static const Color defaultBackgroundColor = Color(0xff111010);
   
 }
 
